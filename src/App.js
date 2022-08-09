82c93e55c93542d941cb88eb4b08f12b69c7416b
unknown <papyszoo@gmail.com>
2022-08-09 23:21:16 +0200

    added cra and sass

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by Papyszoo.
+      </div>
+    </div>
+  );
+}
+
+export default App;
